736bb61869aff6a98389fe9f546d6f53bbc69201
Admin <mramazanov@outlook.com>
2025-05-12 21:55:10 +0300

    init

---
+++ src/main/java/ru/javajabka/teamservice/TeamserviceApplication.java
@@ -0,0 +1,11 @@
+package ru.javajabka.teamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TeamserviceApplication.class, args);
+	}
+}
